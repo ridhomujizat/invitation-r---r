6ec9842aeb9984f8d03526b244dc41765be4b732
ridhotitipku <107243155+ridhotitipku@users.noreply.github.com>
2024-08-09 07:02:45 +0700

    add theme color

--- src/layouts/footer.tsx
+++ src/layouts/footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className="bg-[#976C74] pt-4 pb-12">
+    <div className="bg-[#976C74] pt-4 pb-12 h-[200px]">
       <p className="text-white text-[10px] text-center">Dibuat oleh:</p>
       <p className="text-white text-sm text-center ">Ridho untuk rahma</p>
     </div>
